99c3925365c7cc9e966963e4c24d377e2f3e0568
Ajil Oommen <ajilo297@gmail.com>
2019-02-25 22:57:08 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainApplication());
+
+class MainApplication extends StatefulWidget {
+  @override
+  MainApplicationState createState() => MainApplicationState();
+}
+
+class MainApplicationState extends State<MainApplication> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
